e66fa29a49d9320fc041577444b18e60bcea0106
Yota Toyama <raviqqe@gmail.com>
2018-02-11 18:56:38 +0900

    Add tasks container

---
+++ src/container/tasks.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+import Task from "../component/task";
+import { actionCreators, initialState } from "../state/projects";
+
+class Tasks extends React.Component<typeof initialState> {
+    public render() {
+        const { currentProject, projects } = this.props;
+
+        if (currentProject === null) {
+            return <div>Create a project.</div>;
+        }
+
+        return projects[currentProject].tasks.map((task, key) => <Task key={key} {...task} />);
+    }
+}
+
+export default connect(({ projects }) => projects, actionCreators)(Tasks);
