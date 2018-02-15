1b627faf49dd65eb4cb9b7add4f10a0943f5a4b6
Yota Toyama <raviqqe@gmail.com>
2018-02-15 11:39:37 +0900

    Add LabeledDate component

---
+++ src/component/LabeledDate.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import SubInformation from "./SubInformation";
+
+interface IProps {
+    label: string;
+    value: any;
+}
+
+export default class extends React.Component<IProps> {
+    public render() {
+        const { label, value } = this.props;
+
+        return <SubInformation>{label}: {(new Date(value)).toLocaleDateString()}</SubInformation>;
+    }
+}
