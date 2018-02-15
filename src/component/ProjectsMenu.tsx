152d8fe22cf503f0487d7d8ab40cb1c14f644465
Yota Toyama <raviqqe@gmail.com>
2018-02-15 13:30:10 +0900

    Add ProjectsMenu component

---
+++ src/component/ProjectsMenu.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+import { IProject } from "../domain/project";
+import { actionCreators } from "../state/tasks";
+import ProjectButton from "./ProjectButton";
+import "./style/ProjectsMenu.css";
+
+interface IProps {
+    currentProjectName: string | null;
+    projects: { [name: string]: IProject };
+    setCurrentProjectName: (name: string) => void;
+}
+
+interface IState {
+    opened: boolean;
+}
+
+class ProjectsMenu extends React.Component<IProps, IState> {
+    public state: IState = { opened: false };
+
+    public render() {
+        const { currentProjectName, projects, setCurrentProjectName } = this.props;
+        const { opened } = this.state;
+
+        return (
+            <div className="ProjectsMenu-container">
+                <ProjectButton
+                    className="ProjectsMenu-current-button"
+                    projectName={currentProjectName}
+                    onClick={() => this.setState({ opened: !opened })}
+                />
+                <div
+                    className={"ProjectsMenu-background" + (opened ? "" : "-hidden")}
+                    onClick={() => this.setState({ opened: false })}
+                />
+                <div className={"ProjectsMenu-box" + (opened ? "" : "-hidden")}>
+                    {Object.keys(projects).map((name) =>
+                        <ProjectButton
+                            key={name}
+                            className={name === currentProjectName && "ProjectsMenu-disabled-button"}
+                            projectName={name}
+                            onClick={() => {
+                                setCurrentProjectName(name);
+                                this.setState({ opened: false });
+                            }}
+                        />)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(({ projects }) => projects, actionCreators)(ProjectsMenu);
