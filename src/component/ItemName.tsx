f58ba799d35a347462cca93161b6ee8d154a7882
Yota Toyama <raviqqe@gmail.com>
2018-02-15 11:08:40 +0900

    Add ItemName component

---
+++ src/component/ItemName.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Markdown = require("react-markdown");
+
+import InputComponent, { IProps as IInputComponentProps } from "./InputComponent";
+import "./style/ItemName.css";
+
+interface IProps extends IInputComponentProps {
+    highlighted?: boolean;
+}
+
+export default class extends InputComponent<IProps> {
+    public render() {
+        const { highlighted, onEdit, text } = this.props;
+
+        if (this.state.editing) {
+            return (
+                <input
+                    className="ItemName-container"
+                    onKeyDown={(event: React.KeyboardEvent<{}>) => {
+                        if (event.keyCode === 13) {
+                            this.setState({ editing: false });
+                            event.preventDefault();
+                        }
+                    }}
+                    {...{ ...this.getFormProps() }}
+                />
+            );
+        }
+
+        return (
+            <div
+                className={"ItemName-container" + (highlighted ? "-highlighted" : "")}
+                onClick={() => this.setState({ editing: this.props.onEdit !== undefined })}
+            >
+                {text}
+            </div>
+        );
+    }
+}
