cba2a9cb9f51359e19dc7d597a3c53a558a711d5
Yota Toyama <raviqqe@gmail.com>
2018-02-15 13:26:29 +0900

    Modify props of ProjectButton component

--- src/component/ProjectButton.tsx
+++ src/component/ProjectButton.tsx
@@ -6,17 +6,17 @@ import "./style/ProjectButton.css";
 interface IProps {
     className?: string;
     projectName: string;
-    setCurrentProjectName: (name: string) => void;
+    onClick: () => void;
 }
 
 export default class ProjectButton extends React.Component<IProps> {
     public render() {
-        const { className, projectName, setCurrentProjectName } = this.props;
+        const { className, projectName, onClick } = this.props;
 
         return (
             <NoBoxButton
                 className={className || "ProjectButton-container"}
-                onClick={() => setCurrentProjectName(name)}
+                onClick={onClick}
             >
                 {projectName}
             </NoBoxButton>
