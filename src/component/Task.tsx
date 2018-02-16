3bcf919da154c321066f2d984403232ada83591d
Yota Toyama <raviqqe@gmail.com>
2018-02-16 10:40:07 +0900

    Item -> Task

--- src/component/Task.tsx
+++ src/component/Task.tsx
@@ -1,16 +1,21 @@
 import numeral = require("numeral");
 import * as React from "react";
 import Clock = require("react-icons/lib/md/access-time");
+import Check = require("react-icons/lib/md/check");
+import Trash = require("react-icons/lib/md/delete");
+import Repeat = require("react-icons/lib/md/replay");
 import { connect } from "react-redux";
 
 import { ITask } from "../domain/task";
 import { actionCreators as tasksActionCreators } from "../state/tasks";
 import { actionCreators as timerActionCreators } from "../state/timer";
-import Item from "./Item";
+import ItemName from "./ItemName";
 import LabeledDate from "./LabeledDate";
-import "./style/Task.css";
 import SubInformation from "./SubInformation";
 import TaskDescription from "./TaskDescription";
+import TaskLike from "./TaskLike";
+
+import "./style/Task.css";
 
 interface IProps extends ITask {
     detailed: boolean;
@@ -23,30 +28,34 @@ interface IProps extends ITask {
     toggleTimer: () => void;
 }
 
-class Task extends React.Component<IProps> {
+interface IState {
+    showButtons: boolean;
+}
+
+class Task extends React.Component<IProps, IState> {
+    public state: IState = { showButtons: false };
+
     public render() {
         const {
-            createdAt, detailed, description, setCurrentTaskId,
+            createdAt, detailed, description, id, highlighted, name, setCurrentTaskId,
             spentSeconds, updatedAt, modifyTask,
         } = this.props;
 
         return (
-            <Item
-                {...this.props}
-                buttons={[
-                    <div
-                        key="turnOnTimer"
-                        className="Task-button"
-                        onClick={(event) => {
-                            this.props.setCurrentTaskId(this.task.id);
-                            this.props.toggleTimer();
-                            event.stopPropagation();
-                        }}
-                    >
-                        <Clock />
-                    </div>,
-                ]}
-                details={[
+            <TaskLike
+                onClick={detailed ? undefined : () => setCurrentTaskId(id)}
+                onMouseOver={() => this.setState({ showButtons: true })}
+                onMouseOut={() => this.setState({ showButtons: false })}
+            >
+                <div className="Task-header">
+                    <ItemName
+                        highlighted={highlighted}
+                        onEdit={detailed && ((name) => modifyTask({ ...this.task, name }))}
+                        text={name}
+                    />
+                    {this.buttons}
+                </div>
+                {detailed && [
                     <TaskDescription
                         key="description"
                         text={description}
@@ -56,9 +65,7 @@ class Task extends React.Component<IProps> {
                     <LabeledDate key="createdAt" label="Created on" timeStamp={createdAt} />,
                     <LabeledDate key="updatedAt" label="Updated on" timeStamp={updatedAt} />,
                 ]}
-                item={this.task}
-                onEditName={detailed && ((name) => modifyTask({ ...this.task, name }))}
-            />
+            </TaskLike>
         );
     }
 
@@ -77,6 +84,63 @@ class Task extends React.Component<IProps> {
             <SubInformation key="spentTime">
                 Spent for: {time}
             </SubInformation>
+        );
+    }
+
+    private get buttons() {
+        const { detailed, done, id, removeTask, toggleTaskState } = this.props;
+
+        const buttons: JSX.Element[] = [];
+
+        if (!done || detailed) {
+            buttons.push(
+                <div
+                    key="toggleState"
+                    className="Task-button"
+                    onClick={(event) => {
+                        toggleTaskState(id);
+                        event.stopPropagation();
+                    }}
+                >
+                    {done ? <Repeat /> : <Check />}
+                </div>,
+            );
+        }
+
+        if (!done) {
+            buttons.push(
+                <div
+                    key="turnOnTimer"
+                    className="Task-button"
+                    onClick={(event) => {
+                        this.props.setCurrentTaskId(this.task.id);
+                        this.props.toggleTimer();
+                        event.stopPropagation();
+                    }}
+                >
+                    <Clock />
+                </div>);
+        }
+
+        if (done || detailed) {
+            buttons.push(
+                <div
+                    key="trash"
+                    className="Task-button"
+                    onClick={(event) => {
+                        removeTask(id);
+                        event.stopPropagation();
+                    }}
+                >
+                    <Trash />
+                </div>,
+            );
+        }
+
+        return (
+            <div className={"Task-buttons" + (this.state.showButtons ? "" : "-hidden")}>
+                {buttons}
+            </div>
         );
     }
 }
