b627163d48b011991d570ba72e037eebfc790515
Yota Toyama <raviqqe@gmail.com>
2018-02-17 17:58:48 +0900

    Refactor global style

--- src/component/Link.tsx
+++ src/component/Link.tsx
@@ -3,17 +3,16 @@ import * as React from "react";
 import "./style/Link.css";
 
 interface IProps {
-    colored?: boolean;
     href: string;
 }
 
 export default class extends React.Component<IProps> {
     public render() {
-        const { children, colored, href } = this.props;
+        const { children, href } = this.props;
 
         return (
             <a
-                className={"Link-container" + (colored ? "-colored" : "")}
+                className="Link-container"
                 href={href}
                 onClick={(event) => event.stopPropagation()}
                 target="_blank"
