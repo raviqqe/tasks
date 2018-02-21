fb305a6bb9aa6b00d462bd297c365d32d08cfe80
Yota Toyama <raviqqe@gmail.com>
2018-02-21 15:14:01 +0900

    Test containers

---
+++ src/container/__tests__/Home.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { createStore } from "../../state";
+import Home from "../Home";
+
+test("Render", () => {
+    const { persistor, store } = createStore();
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Home />
+            </PersistGate>
+        </Provider>,
+        document.createElement("div"));
+});
---
+++ src/container/__tests__/Timer.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { createTask } from "../../domain/task";
+import { createStore } from "../../state";
+import { actionCreators } from "../../state/tasks";
+import Timer from "../Timer";
+
+test("Render", () => {
+    const { persistor, store } = createStore();
+
+    const fooTask = createTask("foo", "");
+
+    store.dispatch(actionCreators.addTask(fooTask));
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Timer currentTask={fooTask} />
+            </PersistGate>
+        </Provider>,
+        document.createElement("div"));
+});
