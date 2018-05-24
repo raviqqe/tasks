709553468e122f573550517f0206f207a2536589
Yota Toyama <raviqqe@gmail.com>
2018-05-24 17:04:49 +0900

    Remove deprecated option

--- rakefile.rb
+++ rakefile.rb
@@ -31,7 +31,7 @@ task :run do
 end
 
 task :test do
-  sh 'npx react-scripts-ts test --coverage --env=jsdom --mapCoverage'
+  sh 'npx react-scripts-ts test --coverage --env=jsdom'
 end
 
 task :clean do
