749c678ec6266bb2a248b0543fccb681640ab18a
Yota Toyama <raviqqe@gmail.com>
2018-09-02 00:19:24 -0700

    Use styled-components for small icon buttons

--- src/component/SmallIconButton.tsx
+++ src/component/SmallIconButton.tsx
@@ -1,6 +1,26 @@
 import * as React from "react";
+import styled from "styled-components";
 
-import "./style/SmallIconButton.css";
+import { instantDuration } from "../style/animation";
+import { black, grey, red } from "../style/colors";
+import { windowSmallQuery } from "../style/media";
+
+const Button = styled.div`
+  color: ${black};
+  cursor: pointer;
+  font-size: 1.5em;
+  display: flex;
+  transition: color ${instantDuration}, transform ${instantDuration};
+
+  &:hover {
+    color: ${red};
+    transform: scale(1.5, 1.5);
+  }
+
+  @media ${windowSmallQuery} {
+    color: ${grey};
+  }
+`;
 
 export default ({
   children,
@@ -9,13 +29,12 @@ export default ({
   children?: React.ReactNode;
   onClick: () => void;
 }) => (
-  <div
-    className="SmallIconButton"
+  <Button
     onClick={event => {
       onClick();
       event.stopPropagation();
     }}
   >
     {children}
-  </div>
+  </Button>
 );
