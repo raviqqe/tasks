9ae6a25f92e8463a21c54e4c02f7bba535a36acf
Yota Toyama <raviqqe@gmail.com>
2018-09-02 13:22:28 -0700

    Use styled-components for messages

--- src/component/Message.tsx
+++ src/component/Message.tsx
@@ -1,9 +1,34 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 
 import * as message from "../state/message";
+import { paperBorder } from "../style/border";
+import { yellow } from "../style/colors";
 
-import "./style/Message.css";
+const Message = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  pointer-events: none;
+  z-index: 1000;
+`;
+
+const Box = styled.div<{ shown: boolean }>`
+  ${paperBorder};
+  color: white;
+  cursor: pointer;
+  font-size: 1.2em;
+  background: ${yellow};
+  max-width: 100%;
+  padding: 1em 2em;
+  margin: 1em;
+  transition: transform 0.2s;
+  pointer-events: auto;
+  transform: translateY(${({ shown }) => (shown ? "0px" : "-200%")});
+`;
 
 // Keep previous messages until they go away from screens.
 interface IState {
@@ -24,15 +49,11 @@ export default class extends React.Component<
     const { clearMessage, message } = this.props;
 
     return (
-      <div className="Message">
-        <div
-          className="box"
-          data-hidden={!message}
-          onClick={() => clearMessage()}
-        >
+      <Message>
+        <Box shown={!!message} onClick={() => clearMessage()}>
           {this.state.message}
-        </div>
-      </div>
+        </Box>
+      </Message>
     );
   }
 
