691d17ef046d664fe57b1dba4a1295b457bf67e1
Yota Toyama <raviqqe@gmail.com>
2018-09-02 01:30:38 -0700

    Use styled-components for task names

--- src/component/TaskName.tsx
+++ src/component/TaskName.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import Markdown = require("react-markdown");
+import styled from "styled-components";
 
+import { black, red } from "../style/colors";
 import InputComponent, {
   IProps as IInputComponentProps
 } from "./InputComponent";
 
-import "./style/TaskName.css";
+const Input = styled.input`
+  font-size: 1.1em;
+`;
+
+const Name = styled.div<{ editable: boolean; highlighted: boolean }>`
+  font-size: 1.1em;
+  cursor: ${({ editable }) => (editable ? "text" : "auto")};
+  color: ${({ highlighted }) => (highlighted ? red : black)};
+`;
 
 interface IProps extends IInputComponentProps {
   highlighted?: boolean;
@@ -14,27 +24,20 @@ interface IProps extends IInputComponentProps {
 export default class extends InputComponent<IProps> {
   public render() {
     if (this.state.editing) {
-      return (
-        <input
-          className="TaskName"
-          onKeyDown={this.onEnterKeyDown}
-          {...this.getFormProps()}
-        />
-      );
+      return <Input onKeyDown={this.onEnterKeyDown} {...this.getFormProps()} />;
     }
 
     const { highlighted, onEdit, text } = this.props;
     const editable = !!onEdit;
 
     return (
-      <div
-        className="TaskName"
-        data-editable={editable}
-        data-highlighted={highlighted}
+      <Name
+        editable={editable}
+        highlighted={highlighted}
         onClick={() => this.setState({ editing: editable })}
       >
         {text}
-      </div>
+      </Name>
     );
   }
 }
