69902c457c106dc35f0a811fc591c4147ddb9b51
Yota Toyama <raviqqe@gmail.com>
2018-09-03 02:25:35 -0700

    Use styled-components for projects menu

--- src/component/ProjectsMenu.tsx
+++ src/component/ProjectsMenu.tsx
@@ -1,11 +1,78 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import styled, { css } from "styled-components";
 
 import { IProject, IProjects } from "../domain/project";
 import * as tasks from "../state/tasks";
+import { shortDuration } from "../style/animation";
+import { paperBorder } from "../style/border";
+import { transparentBlack } from "../style/colors";
+import { verticalMargin } from "../style/margin";
+import { windowSmallQuery } from "../style/media";
 import TextButton from "./TextButton";
 
-import "./style/ProjectsMenu.css";
+const CurrentProject = styled.div`
+  color: white;
+  cursor: pointer;
+  font-size: 1.5em;
+  padding: 1em;
+  text-align: center;
+  word-wrap: break-word;
+`;
+
+const Background = styled.div<{ covert: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 100;
+
+  @media ${windowSmallQuery} {
+    left: unset;
+    right: 0;
+  }
+
+  display: ${({ covert }) => (covert ? "none" : "unset")};
+`;
+
+const boxMargin = "1em";
+
+const Box = styled.div<{ covert: boolean }>`
+  ${paperBorder};
+  ${verticalMargin("1.6em")};
+  background: white;
+  padding: 1.5em 2em;
+  position: absolute;
+  left: ${boxMargin};
+  top: ${boxMargin};
+  max-width: calc(100vw - 2 * ${boxMargin});
+  max-height: calc(100vh - 2 * ${boxMargin});
+  overflow: auto;
+  transform-origin: 0 0;
+  transition: ${shortDuration};
+  z-index: 200;
+  box-shadow: 0.2rem 0.2rem 1rem ${transparentBlack};
+
+  @media ${windowSmallQuery} {
+    left: unset;
+    right: ${boxMargin};
+    transform-origin: 100% 0;
+  }
+
+  ${({ covert }) =>
+    covert
+      ? css`
+          transform: scale(0, 0);
+          opacity: 0;
+          visibility: hidden;
+        `
+      : css``};
+`;
+
+const Projects = styled.div`
+  font-size: 1.2em;
+`;
 
 interface IState {
   opened: boolean;
@@ -26,20 +93,16 @@ export default class extends React.Component<
     const { opened } = this.state;
 
     return (
-      <div className="ProjectsMenu">
-        <div
-          className="current-project-name"
-          onClick={() => this.setState({ opened: !opened })}
-        >
+      <div>
+        <CurrentProject onClick={() => this.setState({ opened: !opened })}>
           {currentProjectName}
-        </div>
-        <div
-          className="background"
-          data-hidden={!opened}
+        </CurrentProject>
+        <Background
+          covert={!opened}
           onClick={() => this.setState({ opened: false })}
         />
-        <div className="box" data-hidden={!opened}>
-          <div className="projects">
+        <Box covert={!opened}>
+          <Projects>
             {Object.keys(projects)
               .sort()
               .map(name => (
@@ -54,8 +117,8 @@ export default class extends React.Component<
                   {name}
                 </TextButton>
               ))}
-          </div>
-        </div>
+          </Projects>
+        </Box>
       </div>
     );
   }
