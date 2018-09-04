226e7a40f12a88fab15b2b7e22ea014d7b3a40e4
Yota Toyama <raviqqe@gmail.com>
2018-09-04 00:21:00 -0700

    Format workbox configuration file

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  "globDirectory": "build/",
-  "globPatterns": [
-    "**/*.{json,png,html,js,css}"
-  ],
-  "swDest": "build/service-worker.js",
-  "globIgnores": [
-    "../workbox-cli-config.js"
-  ]
+  globDirectory: "build/",
+  globPatterns: ["**/*.{json,png,html,js,css}"],
+  swDest: "build/service-worker.js",
+  globIgnores: ["../workbox-cli-config.js"]
 };
