cf4f9f0c0f07725d79813d61e30f94f979394c65
Yota Toyama <raviqqe@gmail.com>
2018-09-08 12:52:29 -0700

    Use withInputState HOC in TaskName

--- src/components/TaskName.tsx
+++ src/components/TaskName.tsx
@@ -1,11 +1,12 @@
-import * as React from "react";
+import React, { Component, KeyboardEvent } from "react";
 import styled from "styled-components";
 
 import { black, red } from "../style/colors";
 import Input from "./Input";
-import InputComponent, {
-  IProps as IInputComponentProps
-} from "./InputComponent";
+import withInputState, {
+  IInternalProps,
+  IProps as IInputStateProps
+} from "./with-input-state";
 
 const NameInput = styled(Input)`
   font-size: 1.1em;
@@ -17,29 +18,41 @@ const Name = styled.div<{ editable: boolean; highlighted: boolean }>`
   color: ${({ highlighted }) => (highlighted ? red : black)};
 `;
 
-interface IProps extends IInputComponentProps {
+interface IProps extends IInputStateProps, IInternalProps {
   highlighted?: boolean;
 }
 
-export default class extends InputComponent<IProps> {
-  public render() {
-    if (this.state.editing) {
+export default withInputState(
+  class extends Component<IProps> {
+    public render() {
+      const { editing, inputProps, stopEditing } = this.props;
+
+      if (editing) {
+        return (
+          <NameInput
+            onKeyDown={(event: KeyboardEvent<HTMLInputElement>) => {
+              if (event.keyCode === 13) {
+                stopEditing();
+                event.preventDefault();
+              }
+            }}
+            {...inputProps}
+          />
+        );
+      }
+
+      const { highlighted, onEdit, startEditing, text } = this.props;
+      const editable = !!onEdit;
+
       return (
-        <NameInput onKeyDown={this.onEnterKeyDown} {...this.getFormProps()} />
+        <Name
+          editable={editable}
+          highlighted={highlighted}
+          onClick={() => editable && startEditing()}
+        >
+          {text}
+        </Name>
       );
     }
-
-    const { highlighted, onEdit, text } = this.props;
-    const editable = !!onEdit;
-
-    return (
-      <Name
-        editable={editable}
-        highlighted={highlighted}
-        onClick={() => this.setState({ editing: editable })}
-      >
-        {text}
-      </Name>
-    );
   }
-}
+);
