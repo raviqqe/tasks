7a7a8d7dd795273bce41f266a57462e9207213da
Yota Toyama <raviqqe@gmail.com>
2018-09-08 14:51:27 -0700

    Test TaskName input

--- src/components/__tests__/TaskName.tsx
+++ src/components/__tests__/TaskName.tsx
@@ -1,8 +1,24 @@
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import * as React from "react";
 
 import TaskName from "../TaskName";
 
 test("Shallow mount", () => {
   shallow(<TaskName text="foo" onEdit={() => undefined} />);
+});
+
+test("Input text", () => {
+  let result: string = "";
+
+  const wrapper = mount(
+    <TaskName onEdit={(text: string) => (result = text)} text="foo" />
+  );
+
+  expect(result).toBe("");
+
+  wrapper.simulate("click");
+  wrapper.find("input").simulate("change", { target: { value: "bar" } });
+  wrapper.find("input").simulate("keyDown", { keyCode: 13 });
+
+  expect(result).toBe("bar");
 });
