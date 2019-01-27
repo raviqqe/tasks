bd9a95883821659190c6fdb7535db77fe49df568
Yota Toyama <raviqqe@gmail.com>
2019-01-27 01:50:22 +0000

    Use confirm()

--- src/components/ArchiveProject.tsx
+++ src/components/ArchiveProject.tsx
@@ -1,58 +1,27 @@
 import React, { Component } from "react";
 import { MdArchive } from "react-icons/md";
 import { connect } from "react-redux";
-import styled from "styled-components";
 
 import { IGlobalState } from "../state";
 import * as tasks from "../state/tasks";
-import { normalBorder } from "../style/borders";
 import { grey } from "../style/colors";
-import { verticalMargin } from "../style/margin";
 import IconedButton from "./IconedButton";
-import ModalWindowButton from "./ModalWindowButton";
-
-const Window = styled.div`
-  ${normalBorder};
-  ${verticalMargin("1em")};
-  background: white;
-  padding: 1em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 1.2em;
-`;
 
 class ArchiveProject extends Component<tasks.IState & tasks.IActionCreators> {
   public render() {
     const { archiveProject, currentProjectName } = this.props;
 
     return (
-      <ModalWindowButton
-        buttonComponent={({ openWindow }) => (
-          <IconedButton
-            backgroundColor={grey}
-            icon={<MdArchive />}
-            onClick={openWindow}
-          >
-            archive
-          </IconedButton>
-        )}
+      <IconedButton
+        backgroundColor={grey}
+        icon={<MdArchive />}
+        onClick={() =>
+          confirm(`Are you sure to archive "${currentProjectName}" project?`) &&
+          archiveProject(currentProjectName)
+        }
       >
-        {closeWindow => (
-          <Window>
-            <div>Are you sure to archive "{currentProjectName}" project?</div>
-            <IconedButton
-              icon={<MdArchive />}
-              onClick={() => {
-                archiveProject(currentProjectName);
-                closeWindow();
-              }}
-            >
-              archive
-            </IconedButton>
-          </Window>
-        )}
-      </ModalWindowButton>
+        archive
+      </IconedButton>
     );
   }
 }
--- src/components/DeleteProject.tsx
+++ src/components/DeleteProject.tsx
@@ -1,58 +1,27 @@
 import React, { Component } from "react";
 import { MdDelete } from "react-icons/md";
 import { connect } from "react-redux";
-import styled from "styled-components";
 
 import { IGlobalState } from "../state";
 import * as tasks from "../state/tasks";
-import { normalBorder } from "../style/borders";
 import { grey } from "../style/colors";
-import { verticalMargin } from "../style/margin";
 import IconedButton from "./IconedButton";
-import ModalWindowButton from "./ModalWindowButton";
-
-const Window = styled.div`
-  ${normalBorder};
-  ${verticalMargin("1em")};
-  background: white;
-  padding: 1em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-size: 1.2em;
-`;
 
 class DeleteProject extends Component<tasks.IState & tasks.IActionCreators> {
   public render() {
     const { currentProjectName, removeProject } = this.props;
 
     return (
-      <ModalWindowButton
-        buttonComponent={({ openWindow }) => (
-          <IconedButton
-            backgroundColor={grey}
-            icon={<MdDelete />}
-            onClick={openWindow}
-          >
-            delete
-          </IconedButton>
-        )}
+      <IconedButton
+        backgroundColor={grey}
+        icon={<MdDelete />}
+        onClick={() =>
+          confirm(`Are you sure to delete "${currentProjectName}" project?`) &&
+          removeProject(currentProjectName)
+        }
       >
-        {closeWindow => (
-          <Window>
-            <div>Are you sure to delete "{currentProjectName}" project?</div>
-            <IconedButton
-              icon={<MdDelete />}
-              onClick={() => {
-                removeProject(currentProjectName);
-                closeWindow();
-              }}
-            >
-              delete
-            </IconedButton>
-          </Window>
-        )}
-      </ModalWindowButton>
+        delete
+      </IconedButton>
     );
   }
 }
