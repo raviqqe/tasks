e5474001587642835815aba65cc26917e53e9dd1
Yota Toyama <raviqqe@gmail.com>
2019-08-27 00:10:15 -0700

    Clean up dependencies

--- workbox-cli-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  globDirectory: "build/",
-  globIgnores: ["../workbox-cli-config.js"],
-  globPatterns: ["**/*.{json,png,html,js,css}"],
-  maximumFileSizeToCacheInBytes: 8 * 1024 * 1024,
-  swDest: "build/service-worker.js"
-};
