7c3e4e00b76bc7dd5a612180472ab610699c24ef
Yota Toyama <raviqqe@gmail.com>
2020-04-18 23:47:01 -0700

    Fix task update (#562)

--- src/infrastructure/react/__tests__/Task.tsx
+++ src/infrastructure/react/__tests__/Task.tsx
@@ -1,3 +1,4 @@
+import { fireEvent, render } from "@testing-library/react";
 import React from "react";
 import { create } from "react-test-renderer";
 import { Task } from "../Task";
@@ -8,4 +9,30 @@ it("renders", () => {
       <Task task={{ id: "id", name: "name" }} updateTask={async () => {}} />
     ).toJSON()
   ).toMatchSnapshot();
+});
+
+it("updates a task", () => {
+  jest.spyOn(window, "prompt").mockReturnValue("bar");
+  const updateTask = jest.fn();
+
+  const { container } = render(
+    <Task task={{ id: "", name: "foo" }} updateTask={updateTask} />
+  );
+
+  fireEvent.click(container.querySelector(`[aria-label="Edit"]`) as Element);
+
+  expect(updateTask).toBeCalledTimes(1);
+});
+
+it("does not update any tasks if update is cancelled", () => {
+  jest.spyOn(window, "prompt").mockReturnValue(null);
+  const updateTask = jest.fn();
+
+  const { container } = render(
+    <Task task={{ id: "", name: "foo" }} updateTask={updateTask} />
+  );
+
+  fireEvent.click(container.querySelector(`[aria-label="Edit"]`) as Element);
+
+  expect(updateTask).not.toBeCalled();
 });
