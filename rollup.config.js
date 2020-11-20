cdaea514365ae944c3a8f76796ab9e61f4e7bf6c
Yota Toyama <raviqqe@gmail.com>
2020-11-20 02:06:01 -0800

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
+import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json';
+import nodeResolve from "@rollup/plugin-node-resolve"
 import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'output',
+    dir: 'public',
     format: 'iife',
   },
-  plugins: [json(), typescript()],
+  plugins: [commonjs(), json(), nodeResolve(), typescript()],
 };
 
