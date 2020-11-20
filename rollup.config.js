6c0b7d169b03f7e496a2b053d393883c106d0e66
Yota Toyama <raviqqe@gmail.com>
2020-11-20 02:30:17 -0800

    Fix build again

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,24 @@
-import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json';
-import nodeResolve from "@rollup/plugin-node-resolve"
-import typescript from '@rollup/plugin-typescript';
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    dir: 'public',
-    format: 'iife',
+    dir: "public",
+    format: "iife",
   },
-  plugins: [commonjs(), json(), nodeResolve(), typescript()],
+  plugins: [
+    commonjs(),
+    json(),
+    nodeResolve(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    terser(),
+    typescript(),
+  ],
 };
-
