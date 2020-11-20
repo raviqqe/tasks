1636db7ed3eff85de117dd3a22f93d9c58374fea
Yota Toyama <raviqqe@gmail.com>
2020-11-20 03:59:16 +0000

    Fix error message

--- src/domain/error.ts
+++ src/domain/error.ts
@@ -2,7 +2,7 @@ export function formatErrorMessage(error: Error): string {
   const message: string = error.message;
 
   if (!message[0]) {
-    throw new Error("error message empty");
+    throw new Error("empty error message");
   }
 
   return message[0].toUpperCase() + message.slice(1) + "!";
