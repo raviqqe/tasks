9ed9a807b0ee186496729900b3bd1a2a2e4c5dc8
Yota Toyama <raviqqe@gmail.com>
2020-11-21 12:48:34 -0800

    Use workbox plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
+import { generateSW } from "rollup-plugin-workbox";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
@@ -14,6 +15,10 @@ export default {
   plugins: [
     commonjs(),
     json(),
+    generateSW({
+      swDest: "public/service-worker.js",
+      globDirectory: "public",
+    }),
     nodeResolve(),
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
