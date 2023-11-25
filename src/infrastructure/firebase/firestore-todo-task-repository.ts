0106ee34396433255f44272a288d5ce8ad9f8803
Yota Toyama <raviqqe@gmail.com>
2023-11-25 13:30:35 +0900

    Use loscore (#2170)
    - Uninstall lodash
    - Use loscore

--- src/infrastructure/firebase/firestore-todo-task-repository.ts
+++ src/infrastructure/firebase/firestore-todo-task-repository.ts
@@ -1,3 +1,4 @@
+import { compact, deepEqual } from "@raviqqe/loscore";
 import { type FirebaseApp } from "firebase/app";
 import { type Auth, getAuth } from "firebase/auth";
 import {
@@ -12,7 +13,6 @@ import {
   type Transaction,
   updateDoc,
 } from "firebase/firestore";
-import { compact, isEqual } from "lodash";
 import { type ITodoTaskRepository } from "../../application/todo-task-repository.js";
 import { type ITask } from "../../domain/task.js";
 
@@ -70,7 +70,7 @@ export class FirestoreTodoTaskRepository implements ITodoTaskRepository {
       ];
       const restoredTaskIds: string[] = restoredTasks.map((task) => task.id);
 
-      if (!isEqual(taskIds, restoredTaskIds)) {
+      if (!deepEqual(taskIds, restoredTaskIds)) {
         this.setOrder(projectId, restoredTaskIds, transaction);
       }
       // End of read consistency resolution
