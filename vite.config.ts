ee17383d5462359e3b70a0a8d40ecb122d6cf1cb
Yota Toyama <raviqqe@gmail.com>
2024-12-09 18:30:27 +0900

    Install Vike

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import wywInJs from "@wyw-in-js/vite";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import wywInJs from "@wyw-in-js/vite";
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
         presets: ["@babel/preset-typescript", "@babel/preset-react"],
       },
     }),
+    vike(),
     VitePWA({
       manifest: {
         short_name: "BeDone",
