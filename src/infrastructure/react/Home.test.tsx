0c61ad7af0aab7dbce90dd34c1d025c81796f415
Yota Toyama <raviqqe@gmail.com>
2024-12-12 14:09:02 +0900

    Fix

--- src/infrastructure/react/Home.test.tsx
+++ src/infrastructure/react/Home.test.tsx
@@ -1,6 +1,18 @@
 import { render } from "@testing-library/react";
-import { expect, it } from "vitest";
+import { beforeEach, expect, it, vi } from "vitest";
 import { Home } from "./Home.js";
+import { atom } from "nanostores";
+import { projectPresenter } from "../../main/project-presenter.js";
+
+beforeEach(() => {
+  vi.spyOn(projectPresenter, "currentProject", "get").mockReturnValue(
+    atom({ archived: false, id: "", name: "" }),
+  );
+  vi.spyOn(projectPresenter, "archivedProjects", "get").mockReturnValue(
+    atom([]),
+  );
+  vi.spyOn(projectPresenter, "projects", "get").mockReturnValue(atom([]));
+});
 
 it("renders", () => {
   expect(
