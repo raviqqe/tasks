8d4346ff6d50a55d0a0f9211b9cc1f28878c673b
Yota Toyama <raviqqe@gmail.com>
2024-12-12 13:02:32 +0900

    Fix

--- src/infrastructure/react/ProjectMenu.test.tsx
+++ src/infrastructure/react/ProjectMenu.test.tsx
@@ -1,13 +1,24 @@
 import { render } from "@testing-library/react";
-import { expect, it } from "vitest";
+import { beforeEach, expect, it, vi } from "vitest";
 import { ProjectMenu } from "./ProjectMenu.js";
+import { atom } from "nanostores";
+import { projectPresenter } from "../../main/project-presenter.js";
+
+beforeEach(() => {
+  vi.spyOn(projectPresenter, "currentProject", "get").mockReturnValue(
+    atom({
+      archived: false,
+      id: "",
+      name: "",
+    }),
+  );
+});
 
 it("renders", () => {
   expect(
     render(
       <ProjectMenu
         archivedProjects={[]}
-        currentProject={{ archived: false, id: "", name: "" }}
         onHideProjects={() => {}}
         projects={[]}
       />,
