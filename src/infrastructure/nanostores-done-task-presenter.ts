106c2e737843d0dec85cb832f0e4862680e9e5cc
Yota Toyama <raviqqe@gmail.com>
2024-12-12 17:30:58 +0900

    Fix

--- src/infrastructure/nanostores-done-task-presenter.ts
+++ src/infrastructure/nanostores-done-task-presenter.ts
@@ -2,7 +2,7 @@ import { type DoneTaskPresenter } from "../application/done-task-presenter.js";
 import { type Task } from "../domain/task.js";
 import { type Renderer } from "./renderer.js";
 
-export class DoneTaskRenderer implements DoneTaskPresenter {
+export class NanostoresDoneTaskPresenter implements DoneTaskPresenter {
   private renderer: Renderer | null = null;
   private tasks: Task[] | null = null;
 
