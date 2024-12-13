865cf5eb9be24931171f0381f515d53aa91de750
Yota Toyama <raviqqe@gmail.com>
2024-12-13 16:32:03 +0900

    Fix

--- src/main/todo-task-lister.ts
+++ src/main/todo-task-lister.ts
@@ -1,8 +1,10 @@
 import { TodoTaskLister } from "../application/todo-task-lister.js";
+import { currentProjectRepository } from "./current-project-repository.js";
 import { todoTaskPresenter } from "./todo-task-presenter.js";
 import { todoTaskRepository } from "./todo-task-repository.js";
 
 export const todoTaskLister = new TodoTaskLister(
+  currentProjectRepository,
   todoTaskRepository,
   todoTaskPresenter,
 );
