b3349f346a847ee401190d62ccf3b8950c87ac60
Yota Toyama <raviqqe@gmail.com>
2024-12-13 20:54:12 +0900

    Remove `react-infinite-scroll-hook` (#2724)
    - **Fix**
    - **Fix**
    - **Fix**
    - **Refactor**

--- src/app/components/DoneTaskList.tsx
+++ src/app/components/DoneTaskList.tsx
@@ -1,16 +1,11 @@
 import { styled } from "@linaria/react";
 import { useStore } from "@nanostores/react";
-import { useAsync, usePrevious } from "@raviqqe/react-hooks";
-import { defaultImport } from "default-import";
-import { delay } from "es-toolkit";
-import { useCallback, useEffect, useState } from "react";
-import defaultUseInfiniteScroll from "react-infinite-scroll-hook";
+import { useAsync, useInfiniteScroll } from "@raviqqe/react-hooks";
+import { useCallback, useState } from "react";
 import { doneTaskLister } from "../../main/done-task-lister.js";
 import { doneTaskPresenter } from "../../main/done-task-presenter.js";
 import { Loader } from "./Loader.js";
 import { Task } from "./Task.js";
-
-const useInfiniteScroll = defaultImport(defaultUseInfiniteScroll);
 
 const Container = styled.div`
   display: flex;
@@ -28,42 +23,23 @@ const LoaderContainer = styled.div`
 
 export const DoneTaskList = (): JSX.Element => {
   const tasks = useStore(doneTaskPresenter.tasks);
-  const [loading, setLoading] = useState(false);
   const [done, setDone] = useState(false);
 
   useAsync(() => doneTaskLister.list(), []);
 
-  const onLoadMore = useCallback(async () => {
-    setLoading(true);
-    await doneTaskLister.listMore();
-    await delay(0);
-    setLoading(false);
-  }, [setLoading]);
-
-  useAsync(onLoadMore, []);
-
-  const [ref] = useInfiniteScroll({
-    hasNextPage: !done,
-    loading,
-    onLoadMore,
-  });
-
-  const oldLoading = usePrevious(loading);
-  const [length, setLength] = useState(0);
-
-  useEffect(() => {
-    if (!oldLoading && loading) {
-      setLength(tasks?.length ?? 0);
-    } else if (oldLoading && !loading && tasks?.length === length) {
-      setDone(true);
+  const listMore = useCallback(async () => {
+    if (!tasks) {
+      return;
     }
-  }, [tasks, length, loading, oldLoading]);
+
+    await doneTaskLister.listMore();
+    setDone(doneTaskPresenter.tasks.get()?.length === tasks.length);
+  }, [tasks]);
+  const ref = useInfiniteScroll<HTMLDivElement>(listMore);
 
   return (
     <Container>
-      {(tasks ?? []).map((task) => (
-        <Task key={task.id} task={task} />
-      ))}
+      {tasks?.map((task) => <Task key={task.id} task={task} />)}
       {!done && (
         <LoaderContainer ref={ref}>
           <Loader />
