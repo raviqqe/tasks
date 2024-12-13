861e1968114b2fc7b2c9dbac56a75111a86c8a06
Yota Toyama <raviqqe@gmail.com>
2024-12-13 21:11:53 +0900

    Refactor routes (#2725)
    - **Fix**
    - **Fix**
    - **Fix**

--- src/app/routes/tasks._index/route.tsx
+++ src/app/routes/tasks._index/route.tsx
@@ -1,4 +1,84 @@
+import {
+  closestCenter,
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { styled } from "@linaria/react";
+import { useStore } from "@nanostores/react";
+import { useAsync } from "@raviqqe/react-hooks";
 import { type JSX } from "react";
-import { TodoTaskList } from "../../components/TodoTaskList.js";
+import { todoTaskLister } from "../../../main/todo-task-lister.js";
+import { todoTaskPresenter } from "../../../main/todo-task-presenter.js";
+import { todoTaskReorderer } from "../../../main/todo-task-reorderer.js";
+import { Loader } from "../../components/Loader.js";
+import { Task } from "../../components/Task.js";
+import { buttonMargin } from "../../style.js";
 
-export default (): JSX.Element => <TodoTaskList />;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+`;
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+const Tasks = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 1rem ${buttonMargin};
+  gap: 1rem;
+`;
+
+export default (): JSX.Element => {
+  const todoTasks = useStore(todoTaskPresenter.tasks);
+  const sensors = useSensors(useSensor(PointerSensor));
+
+  useAsync(() => todoTaskLister.list(), []);
+
+  return todoTasks ? (
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={async ({ active, over }) => {
+        if (!over || active.id === over.id) {
+          return;
+        }
+
+        const taskIds = todoTasks.map((task) => task.id);
+
+        await todoTaskReorderer.reorder(
+          arrayMove(
+            taskIds,
+            taskIds.indexOf(active.id.toString()),
+            taskIds.indexOf(over.id.toString()),
+          ),
+        );
+      }}
+      sensors={sensors}
+    >
+      <SortableContext items={todoTasks} strategy={verticalListSortingStrategy}>
+        <Container>
+          <Tasks>
+            {todoTasks.map((task) => (
+              <Task editable key={task.id} task={task} />
+            ))}
+          </Tasks>
+        </Container>
+      </SortableContext>
+    </DndContext>
+  ) : (
+    <LoaderContainer>
+      <Loader />
+    </LoaderContainer>
+  );
+};
--- src/app/routes/tasks.done/route.tsx
+++ src/app/routes/tasks.done/route.tsx
@@ -1,4 +1,50 @@
-import { type JSX } from "react";
-import { DoneTaskList } from "../../components/DoneTaskList.js";
+import { styled } from "@linaria/react";
+import { useStore } from "@nanostores/react";
+import { useAsync, useInfiniteScroll } from "@raviqqe/react-hooks";
+import { useCallback, useState } from "react";
+import { doneTaskLister } from "../../../main/done-task-lister.js";
+import { doneTaskPresenter } from "../../../main/done-task-presenter.js";
+import { Loader } from "../../components/Loader.js";
+import { Task } from "../../components/Task.js";
 
-export default (): JSX.Element => <DoneTaskList />;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  overflow: auto;
+  padding: 1rem;
+`;
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default (): JSX.Element => {
+  const tasks = useStore(doneTaskPresenter.tasks);
+  const [done, setDone] = useState(false);
+
+  useAsync(() => doneTaskLister.list(), []);
+
+  const listMore = useCallback(async () => {
+    if (!tasks) {
+      return;
+    }
+
+    await doneTaskLister.listMore();
+    setDone(doneTaskPresenter.tasks.get()?.length === tasks.length);
+  }, [tasks]);
+  const ref = useInfiniteScroll<HTMLDivElement>(listMore);
+
+  return (
+    <Container>
+      {tasks?.map((task) => <Task key={task.id} task={task} />)}
+      {!done && (
+        <LoaderContainer ref={ref}>
+          <Loader />
+        </LoaderContainer>
+      )}
+    </Container>
+  );
+};
