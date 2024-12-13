41df7713f4055643dd57ff1551ed5f51c953ce4a
Yota Toyama <raviqqe@gmail.com>
2024-12-13 09:17:50 +0900

    Fix

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,3 +1,6 @@
-import { index, type RouteConfig } from "@react-router/dev/routes";
+import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
-export default [index("./routes/index.tsx")] satisfies RouteConfig;
+export default [
+  index("./routes/index.tsx"),
+  route("tasks", "./routes/tasks.tsx"),
+] satisfies RouteConfig;
