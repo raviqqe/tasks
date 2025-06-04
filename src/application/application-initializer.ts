285439bba7b96381cbacac2e7f34d4bfb035da21
Yota Toyama <raviqqe@gmail.com>
2025-06-03 18:18:13 -0700

    Fxi

--- src/application/application-initializer.ts
+++ src/application/application-initializer.ts
@@ -2,10 +2,16 @@ import { type AuthenticationController } from "./authentication-controller.js";
 import { type AuthenticationPresenter } from "./authentication-presenter.js";
 
 export class ApplicationInitializer {
+  private readonly authenticationController: AuthenticationController;
+  private readonly authenticationPresenter: AuthenticationPresenter;
+
   constructor(
-    private readonly authenticationController: AuthenticationController,
-    private readonly authenticationPresenter: AuthenticationPresenter,
-  ) {}
+    authenticationController: AuthenticationController,
+    authenticationPresenter: AuthenticationPresenter,
+  ) {
+    this.authenticationController = authenticationController;
+    this.authenticationPresenter = authenticationPresenter;
+  }
 
   public async initialize(): Promise<void> {
     this.authenticationPresenter.presentSignedIn(
