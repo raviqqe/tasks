7cbcd87b697ad595124c188478526ed788cd83be
Yota Toyama <raviqqe@gmail.com>
2025-08-06 08:48:44 -0700

    Refactor (#3017)

--- src/application/done-task-repository.ts
+++ src/application/done-task-repository.ts
@@ -2,5 +2,5 @@ import { type Task } from "../domain/task.js";
 
 export interface DoneTaskRepository {
   create(projectId: string, task: Task): Promise<void>;
-  list(projectId: string, limit: number): AsyncIterable<Task[]>;
+  list(projectId: string, limit: number): AsyncIterable<Task[], void>;
 }
