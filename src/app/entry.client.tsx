06a809f9f89ed81747b557d2278e671d368e06c6
Yota Toyama <raviqqe@gmail.com>
2026-01-23 23:40:32 -0800

    Fix

--- src/app/entry.client.tsx
+++ src/app/entry.client.tsx
@@ -1,6 +1,15 @@
+import * as sentry from "@sentry/react-router";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
+import { configuration } from "../configuration.json" with { type: "json" };
+
+sentry.init({
+  ...configuration.sentry,
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
 
 startTransition(() => {
   hydrateRoot(
