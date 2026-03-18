a742a363679990d8da587812740ed2ffc7d4c605
Yota Toyama <raviqqe@gmail.com>
2026-03-17 18:48:22 -0700

    Specify coverage provider (#3345)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: "v8",
+    },
     environment: "jsdom",
     include: ["src/**/*.test.ts{,x}"],
     setupFiles: "src/test.ts",
