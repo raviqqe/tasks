343d8b08d5314fffb836e9a42868ee9131efb7b6
Yota Toyama <raviqqe@gmail.com>
2026-06-29 08:37:32 -0700

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,23 @@ export default defineConfig({
   plugins: [
     reactRouter(),
     VitePWA({
-      manifest: false,
+      manifest: {
+        // biome-ignore-start lint/style/useNamingConvention: PWA manifest
+        background_color: "indianred",
+        display: "standalone",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "any",
+            src: "icon.svg",
+          },
+        ],
+        name: "BeDone",
+        short_name: "BeDone",
+        start_url: ".",
+        theme_color: "indianred",
+        // biome-ignore-end lint/style/useNamingConvention: PWA manifest
+      },
       workbox: {
         navigateFallbackDenylist: [/^\/__/],
       },
